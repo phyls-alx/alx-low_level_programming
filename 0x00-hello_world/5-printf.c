5494a700394c3ccc56090154ff3ccd4305381f90
phyls-alx <alx.intranet@gmail.com>
2022-04-07 23:59:08 +0100

    My 6th commit on alx low level programming hello world task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,8 @@
  *Return: return 1 after program execution
  */
 
-  int main(void)
- {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-  }
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
