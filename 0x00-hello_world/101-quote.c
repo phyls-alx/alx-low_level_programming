360eef074d9d367cd68f445e484001d7b39768de
phyls-alx <alx.intranet@gmail.com>
2022-04-08 00:42:43 +0100

    My 9th commit on alx low level programming hello world task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 /**
-*main - Entry point
-*
-*Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
-*
-*Return: Always 0 (Success)
+*main - main block
+*Return: 1
 */
 
 int main(void)
 {
-write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59)
-return (1)
+	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n",s)
+	return (1)
 }
