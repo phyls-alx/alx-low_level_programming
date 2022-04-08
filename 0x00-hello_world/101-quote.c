19829251e401f8331586e9d673807b524ecf36f1
phyls-alx <alx.intranet@gmail.com>
2022-04-08 01:00:05 +0100

    My 9th commit on alx low level programming hello world task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 /**
 *main - main block
@@ -7,8 +6,7 @@
 */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-	write(1,
-		'and that piece of art is useful" - Dora Korpar, 2015-10-19\n', s);
+
+	write(1,'and that piece of art is useful" - Dora Korpar, 2015-10-19\n',59);
 	return (1);
 }
