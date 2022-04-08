6f71ae2681b735d22da70faecccda0930f11662b
phyls-alx <alx.intranet@gmail.com>
2022-04-09 00:34:29 +0100

    My 1st commit on alx low level programming variables_if_else_while task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,20 +11,21 @@
 int main(void)
 {
 int n;
-srand(time(0));
-n=rand()-RAND_MAX/2;
 
-if(n==0)
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n == 0)
 {
-printf("%d is zero\n", n);
+printf("%i is zero\n", n);
 }
-else if(n<0)
+else if (n < 0)
 {
-printf("%d is negative\n", n);
+printf("%i is negative\n", n);
 }
 else
 {
-printf("%d is positive\n", n);
+printf("%i is positive\n", n);
 }
 return (0);
 }
