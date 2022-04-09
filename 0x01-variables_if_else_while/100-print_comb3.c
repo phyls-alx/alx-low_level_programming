0d1879113f3eae90c59aaf2e896824985ac59f33
phyls-alx <alx.intranet@gmail.com>
2022-04-09 02:39:34 +0100

    My 11th commit on alx low level programming variables_if_else_while task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- *
  *main - Print combinations of two digit numbers
+ *
  *Return: Always 0 (Success)
  */
 
@@ -13,16 +13,16 @@ int ones;
 
 for (tens = 0; tens <= 9; tens++)
 {
-	for (ones = tens + 1; ones <= 9; ones++)
-	{
-		putchar(tens + '0');
-		putchar(ones + '0');
-		if (tens < 8)
-		  {
-		    putchar(',');
-		    putchar(' ');
-		  }
-	}
+for (ones = tens + 1; ones <= 9; ones++)
+{
+putchar(tens + '0');
+putchar(ones + '0');
+if (tens < 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
 }
 putchar('\n');
 
