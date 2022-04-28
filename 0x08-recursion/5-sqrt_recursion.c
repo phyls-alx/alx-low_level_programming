f0886fab9da98f6e361776a67fa5e68b29b3f89a
phyls-alx <alx.intranet@gmail.com>
2022-04-28 23:45:21 +0100

    function that returns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,34 +1,45 @@
 #include "main.h"
-
 /**
- *_sqrt_recursion - return the natural square root of a number
- *@n: int number
- *Return: if no natural square root, return -1, Else return natural
- *square root
+ *_evaluate - evaluate function sqrt
+ *@i: interger
+ *@n: interger
+ *Return: evaluate sqrt
  */
 
-int halp(int c, int i)
+int _evaluate(int i, int n)
 {
-	int square;
+/*Evaluate function*/
+if (n == 0 || n == 1)
+return (n);
 
-	square = i * i;
-	if (square == c)
-		return (i);
-	else if (square < c)
-		return (halp(c, i + 1));
-	else
-		return (-1);
+else if (i * i < n)
+return (_evaluate(i + 1, n));
+
+else if (i * i == n) /*Condiction base*/
+return (i);
+
+return (-1);
+
+return (-1);
 }
 
 /**
- * halp - helper function to solve _sqrt_recursion
- * @c: number to determine if square root
- * @i: incrementer to compare against `c`
- * Return: square root if natural square root, or -1 if none found
- */
+ *_sqrt_recursion - evaluate sqrt
+ *@n: interger
+ *Return: Sgrt_recursion
+*/
 
 int _sqrt_recursion(int n)
 {
-	return (halp(n, 1));
+int i = 0;
+
+if (i < 0) /*If n is negative*/
+return (-1);
+
+else
+{
+return (_evaluate(i, n)); /*Recursive call*/
+}
+
 }
 
